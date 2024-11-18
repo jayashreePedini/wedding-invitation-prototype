f9ff144bd8ba844aeff74cbf56da22bce2fca41a
jayashreePedini <jayashree14work@gmail.com>
2024-11-18 18:21:27 +0530

    Make the Project Setup

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import HeadSection from "./components/HeadSection";
+
+const App = () => {
+  return (
+    <main className="w-full h-full bg-[#EDDFE0]">
+      <HeadSection />
+    </main>
+  );
+};
+
+export default App;
---
+++ src/components/HeadSection.tsx
@@ -0,0 +1,5 @@
+const HeadSection = () => {
+  return <section className="header-section w-full h-screen"></section>;
+};
+
+export default HeadSection;
